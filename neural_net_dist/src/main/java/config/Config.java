3c5f0eb3a17c0dd836e37a6c51a9d512acccfa70
Amogh Rathore <amoghdroid09@gmail.com>
2016-12-18 16:23:51 +0900

    done

--- neural_net_dist/src/main/java/config/Config.java
+++ neural_net_dist/src/main/java/config/Config.java
@@ -7,10 +7,10 @@ public class Config {
     public static final int DATA_SIZE = 150;
     public static final double LEARNING_RATE = 0.05;
 
-    public static final int INPUT_LAYER_NEURON_COUNT = 200;
-    public static final int OUTPUT_LAYER_NEURON_COUNT = 15;
+    public static final int INPUT_LAYER_NEURON_COUNT = 784;
+    public static final int OUTPUT_LAYER_NEURON_COUNT = 10;
     public static final int[] LAYER_TO_NEURON = {
-            INPUT_LAYER_NEURON_COUNT, 100, OUTPUT_LAYER_NEURON_COUNT};
+            INPUT_LAYER_NEURON_COUNT, 300, OUTPUT_LAYER_NEURON_COUNT};
 
     public static final int MAX_HIDDEN_LAYER_NUM = 2;
     public static final int INPUT_LAYER = 1;
